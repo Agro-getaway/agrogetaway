b1195c1f6904beaed0ce7458be170caa461d5856
Nalule-Tryphine-Ruth <ruthtryphine@gmail.com>
2023-11-15 17:47:39 +0300

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -52,6 +52,15 @@ const slides = [
     title: "Poultry",
     description: "Lorem ipsum",
   },
+  {
+    image: [
+      "https://images.unsplash.com/photo-1590334280495-2bf3f02ac46d?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1555773443-464ec72c8515?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    ],
+    title: "Apiculture",
+    description: "Lorem ipsum",
+  },
+
   // Add more slides as needed
 ];
 
