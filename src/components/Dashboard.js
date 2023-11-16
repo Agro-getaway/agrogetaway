1c6d3a39d55e16f42050f1f0b2c979d3d64f5fcd
Nalule-Tryphine-Ruth <ruthtryphine@gmail.com>
2023-11-16 18:03:21 +0300

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -84,6 +84,15 @@ const slides = [
     link: "/pomology",
     // description: "Lorem ipsum",
   },
+  {
+    image: [
+      "https://images.unsplash.com/photo-1623408997823-e80db672ce79?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZmlzaCUyMGZhcm1pbmd8ZW58MHx8MHx8fDA%3D",
+      "https://images.unsplash.com/photo-1591121213542-7020cc7a0b52?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    ],
+    title: "Aquaculture",
+    link: "/aquaculture",
+    // description: "Lorem ipsum",
+  },
 
   // Add more slides as needed
 ];
