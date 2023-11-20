d578fb58b06ba122b72fed90dfb2c3b2884c813f
Nalule-Tryphine-Ruth <ruthtryphine@gmail.com>
2023-11-20 16:59:56 +0300

    Update User.js

--- src/components/USerDashboard/User.js
+++ src/components/USerDashboard/User.js
@@ -1,31 +1,23 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-//import Typography from '@mui/material/Typography';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import PropTypes from "prop-types";
+import * as React from "react";
 //import InboxIcon from '@mui/icons-material/MoveToInbox';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 //import MailIcon from '@mui/icons-material/Mail';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
 import { Route, Routes } from "react-router-dom";
-import Dashboard from '../Dashboard';
-import { Label } from '@mui/icons-material';
 
 const drawerWidth = 240;
 
@@ -41,51 +33,55 @@ function ResponsiveDrawer(props) {
     <div>
       <Toolbar />
       <List>
-        {['Dashbaord', 'Model Farmer', 'Schedule Tour', 'Confirm Tour'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton onClick={({key})=>{
-          
-
-            }}>
-              <ListItemIcon>
-               {index % 2 === 0? <DashboardIcon /> : <CalendarMonthIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+        {["Dashbaord", "Model Farmer", "Schedule Tour", "Confirm Tour"].map(
+          (text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton onClick={({ key }) => {}}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <DashboardIcon /> : <CalendarMonthIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          )
+        )}
       </List>
     </div>
   );
 
   // Remove this const when copying and pasting into your project.
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar
         position="fixed"
         sx={{
           width: { sm: `calc(100% - ${drawerWidth}px)` },
           ml: { sm: `${drawerWidth}px` },
-          backgroundColor: "#fff2b2"
+          backgroundColor: "#fff2b2",
         }}
       >
         <Toolbar>
-        <div className="logo">
-          <img
-            src="logo.jpg"
-            alt="Logo"
-            style={{ maxHeight: "50px", maxWidth: "50px", marginRight: "15px" }}
-          />
-        </div>
+          <div className="logo">
+            <img
+              src="logo.jpg"
+              alt="Logo"
+              style={{
+                maxHeight: "50px",
+                maxWidth: "50px",
+                marginRight: "15px",
+              }}
+            />
+          </div>
           <IconButton
             color="inherit"
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
             <MenuIcon />
           </IconButton>
@@ -109,8 +105,11 @@ function ResponsiveDrawer(props) {
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
         >
           {drawer}
@@ -118,8 +117,11 @@ function ResponsiveDrawer(props) {
         <Drawer
           variant="permanent"
           sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
           open
         >
@@ -128,24 +130,30 @@ function ResponsiveDrawer(props) {
       </Box>
       <Box
         component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
       >
         <Toolbar />
-        <Content/>
+        <Content />
       </Box>
     </Box>
   );
 }
 
-function Content(){
-  return <div>
-    <Routes>
-      <Route path="/" element={<div>Dashbaord</div>}></Route>
-      <Route path="/modelFarmer" element={<div>Model Farmer</div>}></Route>
-      <Route path="/scheduleTour" element={<div>Schedule Tour</div>}></Route>
-      <Route path="/confirmTour" element={<div>Confirm Tour</div>}></Route>
-    </Routes>
-  </div>
+function Content() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<div>Dashbaord</div>}></Route>
+        <Route path="/modelFarmer" element={<div>Model Farmer</div>}></Route>
+        <Route path="/scheduleTour" element={<div>Schedule Tour</div>}></Route>
+        <Route path="/confirmTour" element={<div>Confirm Tour</div>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 ResponsiveDrawer.propTypes = {
