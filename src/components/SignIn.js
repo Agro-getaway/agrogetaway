0edd8a21973767b5ac383f2593a0c887c6a53620
Joan Nabirye <107198755+NabiryeJoan@users.noreply.github.com>
2023-11-22 11:10:46 +0300

    🚧

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -55,7 +55,7 @@ export default function SignIn() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
+    const data = new FormData(event.currentTarget); 
     // console.log({
     //   email: data.get("email"),
     //   password: data.get("password"),
