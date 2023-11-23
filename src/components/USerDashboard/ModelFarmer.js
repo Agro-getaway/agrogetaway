06f075cec7283d197d90e858d60c8e0bf3c4849f
Nalule-Tryphine-Ruth <ruthtryphine@gmail.com>
2023-11-23 18:12:16 +0300

    update

--- src/components/USerDashboard/ModelFarmer.js
+++ src/components/USerDashboard/ModelFarmer.js
@@ -1,49 +1,53 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Typography';
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import {
+  default as Grid,
+  default as Typography,
+} from "@mui/material/Typography";
+import * as React from "react";
+import { Link } from "react-router-dom";
 //import { Link } from "react-router-dom";
 
-export default function ImgMediaCard({handleItemClick}) {
-   
-
+export default function ImgMediaCard({ handleItemClick }) {
   return (
-   <Grid
-   container
-   spacing={2}
-   //direction="row"
-   //justifyContent="space-between"
-   //alignItems="flex"
-   display={'flex'}
-   >
-    <Grid item xs={4} style={{ marginRight: 'auto', marginLeft: 'auto' }}>
-    <Card  sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image="OrgFarm.jpg"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-        Kaynera Farm
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        Our passion for organic farming blends tradition and innovation, cultivating sustainable, nutrient-rich produce. 
-        Visit and Learn organic Farming from our Farm.
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" >See Profile</Button>
-      </CardActions>  
-    </Card>
-   </Grid>
+    <Grid
+      container
+      spacing={2}
+      //direction="row"
+      //justifyContent="space-between"
+      //alignItems="flex"
+      display={"flex"}
+    >
+      <Grid item xs={4} style={{ marginRight: "auto", marginLeft: "auto" }}>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            alt="green iguana"
+            height="140"
+            image="OrgFarm.jpg"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Kaynera Farm
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Our passion for organic farming blends tradition and innovation,
+              cultivating sustainable, nutrient-rich produce. Visit and Learn
+              organic Farming from our Farm.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Link to="/profile">
+              <Button size="large">See Profile</Button>
+            </Link>
+          </CardActions>
+        </Card>
+      </Grid>
 
-   <Grid item xs={4} style={{ marginRight: 'auto', marginLeft: 'auto' }}>
+      <Grid item xs={4} style={{ marginRight: "auto", marginLeft: "auto" }}>
         <Card sx={{ maxWidth: 345 }}>
           <CardMedia
             component="img"
@@ -56,17 +60,18 @@ export default function ImgMediaCard({handleItemClick}) {
               Highway Vegetable Farm
             </Typography>
             <Typography variant="body2" color="text.secondary">
-            Gain invaluable insights into sustainable farming practices while enjoying the freshness of our vibrant produce.
-            Book your tour now for an immersive Learning experience.
+              Gain invaluable insights into sustainable farming practices while
+              enjoying the freshness of our vibrant produce. Book your tour now
+              for an immersive Learning experience.
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size="small" >See Profile</Button>
+            <Button size="small">See Profile</Button>
           </CardActions>
         </Card>
       </Grid>
 
-      <Grid item xs={4} style={{ marginRight: 'auto', marginLeft: 'auto' }}>
+      <Grid item xs={4} style={{ marginRight: "auto", marginLeft: "auto" }}>
         <Card sx={{ maxWidth: 345 }}>
           <CardMedia
             component="img"
@@ -76,20 +81,19 @@ export default function ImgMediaCard({handleItemClick}) {
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
-             Cocoa Farm 
+              Cocoa Farm
             </Typography>
             <Typography variant="body2" color="text.secondary">
-            Discover the art of cultivating fine cocoa beans and learn the secrets behind our rich, velvety chocolates.
-            Book your tour today and step into the flavorful world of SweetBloom Farm.
+              Discover the art of cultivating fine cocoa beans and learn the
+              secrets behind our rich, velvety chocolates. Book your tour today
+              and step into the flavorful world of SweetBloom Farm.
             </Typography>
           </CardContent>
           <CardActions>
-            
-            <Button size="small" >See Profile</Button>
-    
+            <Button size="small">See Profile</Button>
           </CardActions>
         </Card>
       </Grid>
-  </Grid>  
+    </Grid>
   );
 }
