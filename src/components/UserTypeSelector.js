ce625b802a5476d6166792da4b5f3e82d78f4d71
Joan Nabirye <107198755+NabiryeJoan@users.noreply.github.com>
2023-11-24 20:56:22 +0300

    🎯

--- src/components/UserTypeSelector.js
+++ src/components/UserTypeSelector.js
@@ -7,8 +7,10 @@ import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 
 const Profile = () => {
+  const navigate = useNavigate();
   const farmDetails = {
     // ... (your farmDetails object)
+    
   };
 
   const sliderSettings = {
