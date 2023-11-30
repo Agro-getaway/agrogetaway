c64405a69cb5d4bef55772d418fb55f06a9d75fd
Josephine <josephinenamu98@gmail.com>
2023-11-30 21:11:13 +0300

    changes on the booking page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import SignIn from "./components/SignIn.js";
 import SignUp from "./components/SignUp.js";
 import Book from "./components/USerDashboard/Book.js";
 import Profile from "./components/USerDashboard/Profile.js";
-import User from "./components/USerDashboard/User.js";
+// import User from "./components/USerDashboard/User.js";
 import Selector from "./components/UserTypeSelector.js";
 function App() {
   // const [isLogged, setIsLogged] = React.useState(true);
@@ -25,7 +25,7 @@ function App() {
         {/* {isLogged &&  */}
         <Route path="/explore" element={<Explore />}></Route>
         <Route path="/logout" element={<LogOut />}></Route>
-        <Route path="/users" element={<User />}></Route>
+        {/* <Route path="/users" element={<User />}></Route> */}
         <Route path="/book" element={<Book />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
         <Route path="/selector" element={<Selector />}></Route>
