01f240bd7e305adc93af9ef811145ae64e06f54f
Nalule-Tryphine-Ruth <ruthtryphine@gmail.com>
2023-12-01 12:11:19 +0300

    update

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import SignIn from "./components/SignIn.js";
 import SignUp from "./components/SignUp.js";
 import Book from "./components/USerDashboard/Book.js";
 import Profile from "./components/USerDashboard/Profile.js";
-
 import Messaging from "./components/USerDashboard/message.js";
+import Poultry from "./components/USerDashboard/poultry.js";
 import FarmTypes from "./components/USerDashboard/types.js";
 import Selector from "./components/UserTypeSelector.js";
 function App() {
@@ -24,12 +24,13 @@ function App() {
         <Route path="/signin" element={<SignIn />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/farms" element={<Farms />}></Route>
+        <Route path="/poultry" element={<Poultry />}></Route>
         <Route path="/dashboard" element={<Types />}></Route>
         {/* {isLogged &&  */}
         <Route path="/explore" element={<Explore />}></Route>
         <Route path="/logout" element={<LogOut />}></Route>
-      
-        <Route path="/book" element={<Book />}></Route> 
+
+        <Route path="/book" element={<Book />}></Route>
         <Route path="/types" element={<FarmTypes />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
         <Route path="/selector" element={<Selector />}></Route>
