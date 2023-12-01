102a14e3772274a22551c7f198e42065c1400c3d
Nalule-Tryphine-Ruth <ruthtryphine@gmail.com>
2023-12-01 12:19:15 +0300

    Update Farms.js

--- src/components/Farms.js
+++ src/components/Farms.js
@@ -86,7 +86,7 @@ const farmsData = [
       "https://images.unsplash.com/photo-1602083158217-c608d17c44e5?q=80&w=1635&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    name: "Biyinzika Rooster Ranch",
+    name: "Biyinzika Poultry Farm",
     location: "Kampala",
     method: "Poultry Farming",
     imageUrl:
