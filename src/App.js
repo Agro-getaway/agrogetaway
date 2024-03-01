48d8a06103f14d1158a85c36da11e9692c2df6f9
Nalule-Tryphine-Ruth <ruthtryphine@gmail.com>
2024-03-01 18:38:53 +0300

    neew

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Book from "./components/Book.js";
-import Types from "./components/Dash.js";
-import Explore from "./components/Dashboard.js";
-import Farms from "./components/Farms.js";
-import LandingPage from "./components/Home.js";
-import Profile from "./components/Profile.js";
-import SignIn from "./components/SignIn.js";
-import SignUp from "./components/SignUp.js";
-import AboutUs from "./components/aboutus.js";
-import Poultry from "./components/poultry.js";
-import FarmTypes from "./components/types.js";
-import UserProfile from "./components/user.js";
+import Home from "./components/Home";
+// import Book from "./components/Book.js";
+// import Types from "./components/Dash.js";
+// import Explore from "./components/Dashboard.js";
+// import Farms from "./components/Farms.js";
+// import LandingPage from "./components/Home.js";
+// import Profile from "./components/Profile.js";
+// import SignIn from "./components/SignIn.js";
+// import SignUp from "./components/SignUp.js";
+// import AboutUs from "./components/aboutus.js";
+// import Poultry from "./components/poultry.js";
+// import FarmTypes from "./components/types.js";
+// import UserProfile from "./components/user.js";
 function App() {
   // const [isLogged, setIsLogged] = React.useState(true);
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        {/* <Route path="/" element={<LandingPage />}></Route>
         <Route path="/signin" element={<SignIn />}></Route>
         <Route path="/aboutus" element={<AboutUs />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
@@ -26,11 +28,11 @@ function App() {
         <Route path="/poultry" element={<Poultry />}></Route>
         <Route path="/dashboard" element={<Types />}></Route>
         {/* {isLogged &&  */}
-        <Route path="/explore" element={<Explore />}></Route>
+        {/* <Route path="/explore" element={<Explore />}></Route>
         <Route path="/book" element={<Book />}></Route>
         <Route path="/types" element={<FarmTypes />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
-        <Route path="/userprofile" element={<UserProfile />}></Route>
+        <Route path="/userprofile" element={<UserProfile />}></Route> */}
       </Routes>
     </>
   );
